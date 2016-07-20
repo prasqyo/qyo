9bf8b78fb6906d35bf90b0d51854ef7780d88a03
Gunawan10 <gupuspo@gmail.com>
2016-07-20 16:05:54 +0700

    perubahan sedikit di menu simpanan wajib cetak

--- application/views/konten/laporan/cetaksimpananwajib.php
+++ application/views/konten/laporan/cetaksimpananwajib.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Koperasi DKB | Cetak data anggota</title>
+    <title>Koperasi DKB | Cetak simpanan wajib</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
@@ -29,38 +29,39 @@
           <div class="col-xs-12">
             <h2 class="page-header">
               <img src="<?php echo base_url();?>assetdata/images/logo2.png" alt="" width="24" height="24">  Detail Transaksi
-              <small class="pull-right">Date: 2/10/2014</small>
+              <small class="pull-right">Date: <?php date_default_timezone_set('Asia/Jakarta');
+      $getdatetime = date('m/d/Y'); echo $getdatetime; ?></small>
             </h2>
           </div><!-- /.col -->
         </div>
         <!-- info row -->
         <div class="row invoice-info">
-          <div class="col-sm-5 invoice-col">
-          <table width="100%">
+          <div class="col-sm-12 invoice-col">
+          <table>
             <tr>
-              <td width="50">Nomer Anggota</td>
-              <td width="20" class="text-center">:</td>
-              <td width="100"><?php echo $noanggota; ?></td>
+              <td width="180">Nomer Anggota</td>
+              <td width="40" class="text-center">:</td>
+              <td width="200"><?php echo $noanggota; ?></td>
             </tr>
             <tr>
-              <td width="50">NIK</td>
-              <td width="20" class="text-center">:</td>
-              <td width="100"><?php echo $nik; ?></td>
+              <td width="180">NIK</td>
+              <td width="40" class="text-center">:</td>
+              <td width="200"><?php echo $nik; ?></td>
             </tr>
             <tr>
-              <td width="50">Nama Anggota</td>
-              <td width="20" class="text-center">:</td>
-              <td width="100"><?php echo $namaanggota; ?></td>
+              <td width="180">Nama Anggota</td>
+              <td width="40" class="text-center">:</td>
+              <td width="200"><?php echo $namaanggota; ?></td>
             </tr>
             <tr>
-              <td width="50">Unit Kerja</td>
-              <td width="20" class="text-center">:</td>
-              <td width="100"><?php echo $unit; ?></td>
+              <td width="180">Unit Kerja</td>
+              <td width="40" class="text-center">:</td>
+              <td width="200"><?php echo $unit; ?></td>
             </tr>
             <tr>
-              <td width="50">Jenis Simpanan</td>
-              <td width="20" class="text-center">:</td>
-              <td width="100">Simpanan Wajib</td>
+              <td width="180">Jenis Simpanan</td>
+              <td width="40" class="text-center">:</td>
+              <td width="200">Simpanan Wajib</td>
             </tr>
           </table>
           </div><!-- /.col -->
@@ -70,26 +71,26 @@
         <div class="row">
           <div class="col-xs-12 table-responsive">
             <table class="table table-striped table-bordered">
-                            <thead>
-                              <tr>
-                                <th>Kode Transaksi</th>
-                                <th>Bulan</th>
-                                <th>Tahun</th>
-                                <th>Tanggal Transaksi</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                              <?php foreach ($detailsimpanan as $fetchdata) {
-                              ?>
-                              <tr>
-                                <td><?php echo $fetchdata['kode_transaksi'];?></td>
-                                <td><?php echo $fetchdata['Bulan'];?></td>
-                                <td><?php echo $fetchdata['Tahun'];?></td>
-                                <td><?php echo $fetchdata['tanggal_transaksi'];?></td>
-                              </tr>
-                              <?php } ?>
-                            </tbody>
-                          </table>
+              <thead>
+                <tr>
+                  <th>Kode Transaksi</th>
+                  <th>Bulan</th>
+                  <th>Tahun</th>
+                  <th>Tanggal Transaksi</th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php foreach ($detailsimpanan as $fetchdata) {
+              ?>
+                <tr>
+                  <td><?php echo $fetchdata['kode_transaksi'];?></td>
+                  <td><?php echo $fetchdata['Bulan'];?></td>
+                  <td><?php echo $fetchdata['Tahun'];?></td>
+                  <td><?php echo $fetchdata['tanggal_transaksi'];?></td>
+                </tr>
+              <?php } ?>
+              </tbody>
+          </table>
           </div><!-- /.col -->
         </div><!-- /.row -->
 
