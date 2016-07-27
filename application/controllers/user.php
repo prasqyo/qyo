c0a3e242561066ad060495cab7228ae0c5a94044
Gunawan10 <gupuspo@gmail.com>
2016-07-27 11:16:10 +0700

    crud user

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -10,27 +10,97 @@ class User extends CI_Controller {
  		$this->load->library('session');
  	}
 
+ 	//fungsi ini untuk generate message
+ 	public function message($mode,$text,$active)
+ 	{
+ 		//generate message
+ 		$messagesession = array(
+ 			'messagemode' => $mode,
+ 			'messagetext' => $text,
+ 			'messageactive' => $active);
+
+ 		$this->session->set_flashdata($messagesession);
+ 	}
+
+ 	
  	public function index(){
+ 		$data['user'] = $this->global_model->query("select *from user where Level !='1'");
 		//load view
  		$this->load->view('head/dashboard/index');
- 		$this->load->view('konten/user/index');
+ 		$this->load->view('konten/user/index',$data);
  		$this->load->view('footer/dashboard/index');
 
  	}
 	
 	public function tambah(){
-		//load view
-		$this->load->view('head/dashboard/index');
- 		$this->load->view('konten/user/tambah');
- 		$this->load->view('footer/dashboard/index');
+		if($this->input->post('simpan')){
+			/*generate kode user */
+ 			$sql = $this->global_model->query("select *from user order by kode_user desc");
+ 			$kode = "USR";
+
+ 			if($sql != Null){
+ 				$pisah = explode('-', $sql[0]['kode_user']);
+
+ 				$number =  (int) $pisah[1];
+ 				$digit = intval($number) + 1;
+
+ 				if ($digit >= 1 and $digit <= 9){
+					$a = $kode."-00".$digit;
+	 			}else if($digit >= 10 and $digit <= 99){
+	 				$a = $kode."-0".$digit;
+	 			}else{
+	 				$a = $kode."-".$digit;
+	 			}
+
+ 			}else{
+ 				$kodedefault = "USR-001";
+ 				$a = $kodedefault;
+ 			}
+ 			/* akhir generate kode user*/
+
+			$data = $this->input->post();
+			$data['kode_user'] = $a;
+			$data['Password'] = md5($this->input->post('Password'));
+			unset($data['simpan']);
+			$this->global_model->create('user',$data);
+			$this->message('success','Data berhasil di tambah','indexuser');
+			redirect(site_url('user'));
+		}
 		
 	}
 
-	public function edit(){
-		//load view
-		$this->load->view('head/dashboard/index');
- 		$this->load->view('konten/user/edit');
- 		$this->load->view('footer/dashboard/index');
-		
+	public function edit($id){
+		if($this->input->post('simpan')){
+			$data = $this->input->post();
+			$data['Password'] = md5($this->input->post('Password'));
+			unset($data['simpan']);
+			if($this->input->post('Password') == ""){
+				unset($data['Password']);
+			}
+			$this->global_model->update('user',$data, array('kode_user' => $id));
+			$this->message('success','Data berhasil di edit','indexuser');
+			redirect(site_url('user'));
+		}
 	}
+
+	public function hapus(){
+		$chkbox = $this->input->post('check');
+	 	if(is_array($chkbox)){
+	 		for($i = 0; $i < count($chkbox); $i++){
+	 			$this->global_model->delete('user', array('kode_user' => $chkbox[$i]));
+
+	 		}
+	 	}
+
+	 	//memberikan pesan
+ 		$this->message('success','Data berhasil di hapus','indexuser');
+
+	 	redirect(site_url('user'));
+	}
+
+	public function tampil($id){
+ 		$sql = $this->global_model->find_by('user', array('kode_user' => $id));
+ 		echo json_encode($sql);
+ 	}
+
 }
--- application/views/konten/user/index.php
+++ application/views/konten/user/index.php
@@ -1,13 +1,13 @@
           <div class="">
             <div class="page-title">
               <div class="title_left">
-                <h3>User <small>List user</small></h3>
+                <h3><a href="<?php echo base_url();?>index.php/user">User</a> <small>List user</small></h3>
               </div>
 
               <div class="title_right">
                 <div class="form-group pull-right">
                   <div class="input-group">
-                    <a href="<?php echo base_url();?>index.php/user/tambah" class="btn btn-primary"><i class="fa fa-plus-square"></i> Tambah data</a>
+                    <button class="tambahbutton btn btn-primary" onclick="openmodal('tambah')"><i class="fa fa-plus"></i> Tambah data</button>
                     <button class="btn btn-danger" onclick="openmodal('hapus')"><i class="fa fa-trash"></i> Hapus data</button>
                   </div>
                 </div>
@@ -25,27 +25,38 @@
                       <thead>
                         <tr>
                           <th width="20"><input type="checkbox" onclick="for(c in document.getElementsByName('check[]')) document.getElementsByName('check[]').item(c).checked =  this.checked"></th>
-                          <th>Kode Anggota</th>
-                          <th>NIK</th>
-                          <th>Nama Anggota</th>
-                          <th>Unit Kerja</th>
-                          <th>Tanggal Bergabung</th>
+                          <th>Kode user</th>
+                          <th>Nama Lengkap</th>
+                          <th>Username</th>
+                          <th>Level</th>
                           <th class="text-center">Action</th>
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td><input type="checkbox" name="check[]" value=""></td>
-                          <td>-</td>
-                          <td>-</td>
-                          <td>-</td>
-                          <td>-</td>
-                          <td>-</td>
+                        <?php foreach ($user as $fetchdata) {
+                        ?>
+                        <tr class="record">
+                          <td><input type="checkbox" name="check[]" value="<?php echo $fetchdata['kode_user'];?>"></td>
+                          <td id="kode"><?php echo $fetchdata['kode_user'];?></td>
+                          <td><?php echo $fetchdata['Nama_Lengkap'];?></td>
+                          <td><?php echo $fetchdata['Username'];?></td>
+                          <td>
+                          <?php
+                          $ket = "Karyawan";
+                          if($fetchdata['Level'] == "1"){
+                            $ket = "Admin";
+                          }else if($fetchdata['Level'] == "2"){
+                            $ket = "Ketua";
+                          }else if($fetchdata['Level'] == "3"){
+                            $ket = "Pengurus";
+                          }
+                          echo $ket;?>
+                          </td>
                           <td class="text-center">
-                            <button type="button" class="btn btn-success btn-xs" data-placement="top" data-toggle="tooltip" title="View" onclick="openmodal('view')"><span class="glyphicon glyphicon-eye-open"></span></button>
-                            <a href="<?php echo base_url();?>index.php/user/edit" class="btn btn-default btn-xs" data-placement="top" data-toggle="tooltip" title="Edit"><span class="glyphicon glyphicon-pencil"></span></a>
+                            <button type="button" class="editbutton btn btn-default btn-xs" data-placement="top" data-toggle="tooltip" title="Edit"><span class="glyphicon glyphicon-pencil"></span></button>
                           </td>
                         </tr>
+                        <?php } ?>
                       </tbody>
                     </table>
                     </form>
@@ -67,35 +78,129 @@
                     <div class="modal-footer">
                       <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
                       <button type="submit" form="myform" class="btn btn-danger">Hapus</button>
-                      </form>
                     </div>
                   </div><!-- /.modal-content -->
                 </div><!-- /.modal-dialog -->
             </div><!-- /.modal -->
 
-            <div class="modal fade" id="view">
-                <div class="modal-dialog">
+            <div class="modal fade" id="tambah">
+                <div class="modal-dialog modal-sm">
                   <div class="modal-content">
                     <div class="modal-header">
                       <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                      <h4 class="modal-title">View Data</h4>
+                      <h4 class="modal-title">Tambah Data</h4>
                     </div>
+                    <form method="post" action="<?php echo base_url();?>index.php/user/tambah">
                     <div class="modal-body">
-                      <p>Apa anda yakin ingin menghapus data tersebut?</p>
+                      <div class="form-group">
+                        <label>Nama Lengkap</label>
+                        <input type="text" name="Nama_Lengkap" class="form-control" id="namatambah" required>
+                      </div>
+                      <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="Username" class="form-control" id="usernametambah" required>
+                      </div>
+                      <div class="form-group">
+                        <label>Password</label>
+                        <input type="text" name="Password" class="form-control" id="passwordtambah" required>
+                      </div>
+                      <div class="form-group">
+                        <label>Level</label>
+                        <select class="form-control" name="Level" required>
+                          <option></option>
+                          <option value="2">Ketua</option>
+                          <option value="3">Pengurus</option>            
+                        </select>
+                      </div>
                     </div>
                     <div class="modal-footer">
                       <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
-                      <button type="submit" form="myform" class="btn btn-danger">Hapus</button>
-                      </form>
+                      <input type="submit" class="btn btn-primary" name="simpan" value="Simpan">
                     </div>
+                    </form>
+                  </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
+
+            <div class="modal fade" id="edit">
+                <div class="modal-dialog modal-sm">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title">Edit Data</h4>
+                    </div>
+                    <form method="post" id="editform">
+                    <div class="modal-body">
+                      <div class="form-group">
+                        <label>Nama Lengkap</label>
+                        <input type="text" name="Nama_Lengkap" class="form-control" id="namaedit" required>
+                      </div>
+                      <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="Username" class="form-control" id="usernameedit" required>
+                      </div>
+                      <div class="form-group">
+                        <label>Password (isi jika diubah)</label>
+                        <input type="text" name="Password" class="form-control" id="passwordedit">
+                      </div>
+                      <div class="form-group">
+                        <label>Level</label>
+                        <select class="form-control" name="Level" id="leveledit" required>
+                          <option></option>
+                          <option value="2">Ketua</option>
+                          <option value="3">Pengurus</option>            
+                        </select>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                      <input type="submit" class="btn btn-primary" name="simpan" value="Edit">
+                    </div>
+                    </form>
                   </div><!-- /.modal-content -->
                 </div><!-- /.modal-dialog -->
             </div><!-- /.modal -->
 
           </div>
 
+<?php
+if($this->session->flashdata('messagemode','messagetext','messageactive') && $this->session->flashdata('messageactive') == "indexuser"){
+echo "<script type='text/javascript'>";
+  echo "$(document).ready(function() {";
+    echo "new PNotify({";
+      echo "title: 'Informasi !',";
+      echo "text: '".$this->session->flashdata('messagetext')."',";
+      echo "type: '".$this->session->flashdata('messagemode')."',";
+      echo "styling: 'bootstrap3'";
+    echo "});";
+  echo "});";
+echo "</script>";
+}
+?>
+
 <script type="text/javascript">
 function openmodal(id){
   $('#'+id).modal('show');
 }
-</script>          
+</script>
+<script type="text/javascript">
+    $(".editbutton").click(function(event) {
+        var record = $(this).parents('.record');
+
+        $.getJSON('<?php echo base_url(); ?>index.php/user/tampil/'+record.find('#kode').html(), function(data) {
+        $("#namaedit").val(data.Nama_Lengkap);
+        $("#usernameedit").val(data.Username);
+        $("#leveledit").val(data.Level);
+        $("#editform").attr("action", "<?php echo base_url(); ?>/index.php/user/edit/"+record.find('#kode').html());
+    });
+
+        $('#edit').modal('show');
+    
+    });
+
+    $(".tambahbutton").click(function(event) {
+        $("#namatambah").val('');
+        $("#passwordtambah").val('');
+        $("#usernametambah").val('');
+    });
+</script>
