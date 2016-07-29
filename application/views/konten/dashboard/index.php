cbc8e9cad30265d65d549ed75c21889e85732587
Gunawan10 <gupuspo@gmail.com>
2016-07-29 09:46:56 +0700

    load jumlah data di dashboard

--- application/views/konten/dashboard/index.php
+++ application/views/konten/dashboard/index.php
@@ -1,33 +1,65 @@
 <!-- top tiles -->
 <div class="row tile_count">
   <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-    <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
-    <div class="count green">0</div>
+    <span class="count_top"><i class="fa fa-user"></i> Banyak Data Anggota</span>
+    <div class="count green">
+      <?php
+      $getcount = count($this->global_model->find_all('anggota'));
+      echo $getcount;
+      ?>
+    </div>
     <span class="count_bottom">Koperasi DKB Pusat</span>
   </div>
   <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-    <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>
-    <div class="count green">0</div>
+    <span class="count_top"><i class="fa fa-clock-o"></i> Banyak Data Pengurus</span>
+    <div class="count green">
+      <?php
+      $getcount = count($this->global_model->find_all('pengurus'));
+      echo $getcount;
+      ?>
+    </div>
     <span class="count_bottom">Koperasi DKB Pusat</span>
   </div>
   <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-    <span class="count_top"><i class="fa fa-user"></i> Total Males</span>
-    <div class="count green">0</div>
+    <span class="count_top"><i class="fa fa-user"></i> Banyak Data Simpanan</span>
+    <div class="count green">
+      <?php
+      $getcount = count($this->global_model->find_all('simpanpokok'));
+      $getcount2 = count($this->global_model->find_all('simpanwajib'));
+      $getcount3 = count($this->global_model->find_all('simpansukarela'));
+      echo intval($getcount+$getcount2+$getcount3);
+      ?>
+    </div>
     <span class="count_bottom">Koperasi DKB Pusat</span>
   </div>
   <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-    <span class="count_top"><i class="fa fa-user"></i> Total Females</span>
-    <div class="count green">0</div>
+    <span class="count_top"><i class="fa fa-user"></i> Banyak Data Pinjaman</span>
+    <div class="count green">
+      <?php
+      $getcount = count($this->global_model->find_all('pinjaman'));
+      echo $getcount;
+      ?>
+    </div>
     <span class="count_bottom">Koperasi DKB Pusat</span>
   </div>
   <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-    <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>
-    <div class="count green">0</div>
+    <span class="count_top"><i class="fa fa-user"></i> Banyak Data Angsuran</span>
+    <div class="count green">
+      <?php
+      $getcount = count($this->global_model->find_all('angsuran_pinjam'));
+      echo $getcount;
+      ?>
+    </div>
     <span class="count_bottom">Koperasi DKB Pusat</span>
   </div>
   <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
-    <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>
-    <div class="count green">0</div>
+    <span class="count_top"><i class="fa fa-user"></i> Banyak Data User</span>
+    <div class="count green">
+      <?php
+      $getcount = count($this->global_model->find_all('user'));
+      echo $getcount;
+      ?>
+    </div>
     <span class="count_bottom">Koperasi DKB Pusat</span>
   </div>
 </div>
